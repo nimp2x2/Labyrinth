5cd58a56880a45a8da60f4b74d3ba56a22e24216
nimp2x2 <nimp2x2@gmail.com>
2016-06-29 01:21:00 +0300

    Update main.cpp (reverted from commit ba25dd9a7f0e809144b2de3729d74e41fa1da9e9) (reverted from commit 588cac3d5af92c4846b4a30482939ba3302bcbdd) (reverted from commit 7a06bf0a7a8ccd49b0fc2db78a169d0542d87e8d)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 INT WINAPI wWinMain( HINSTANCE hInst, HINSTANCE, LPWSTR, INT )
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetBreakAlloc(3000); // остановка на выделение текущей памяти
 
 	if(engineHeaders.Init(hInst))
 	{
